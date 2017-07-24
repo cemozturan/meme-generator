62720a1c5b0ccb586beecbf9fc5a68ea41766fc4
Cem Ozturan <cozturan@findmypast.com>
2017-07-24 17:27:02 +0100

    Adding some custom animation to memes (text fades in, and image darkens on hover over 1 second)

--- src/components/meme-item.js
+++ src/components/meme-item.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class MemeItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hovered: false
+    };
+  }
+
   render() {
     return (
-      <div className="meme-item">
+      <div
+        className="meme-item"
+        onMouseEnter={() => this.setState({ hovered: true })}
+        onMouseLeave={() => this.setState({ hovered: false })}>
         <img
-          className="meme-image"
+          className={this.state.hovered ? "meme-image darken-image" : "meme-image"}
           src={this.props.meme.url}
           alt={this.props.meme.name} />
-        <p className="meme-text">{this.props.meme.name}</p>
+        <p className={this.state.hovered ? "meme-text" : "no-text"}>{this.props.meme.name}</p>
       </div>
     );
   }
